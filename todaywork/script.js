97e1560d91785bd78f5657fbac64397f29c8706e
sammydavid <sammydavid215@gmail.com>
2021-06-25 11:47:27 +0100

    index.html

--- todaywork/script.js
+++ todaywork/script.js
@@ -2,4 +2,72 @@
         function myFunction(){
             document.getElementById("demo").innerHTML = "paragraph change"
         }
-   
+       // window.alert("Front End Class")
+   document.write("Oge")
+   var A=2;
+   var B=2;
+   var C=2;
+   var D=A + B;
+   //console
+   console.log("D")
+
+    var pi = "undefined";
+    var person = "name";
+    var answer = "number";
+    var RightWrong = "Boolean";
+    var numberChildren = "null";
+    //var total = price1 + undefined;
+//console
+    console.log("person");
+    console.log("answer");
+    console.log("RightWrong");
+    console.log("numberchildren");
+    console.log("pi");
+
+   // innerHTML
+document.getElementById("name").innerHTML = person;
+document.getElementById("number").innerHTML = answer;
+document.getElementById("boolean").innerHTML = RightWrong;
+document.getElementById("null").innerHTML = numberChildren;
+document.getElementById("undefined").innerHTML = pi;
+
+//variable declearation
+var A=2;
+var B=2;
+var C=2;
+var multiple = A*B*C;
+
+//console
+console.log(multiple);
+
+//variable declearation
+var A=2;
+var B=2;
+var C=2;
+var Add = A+B+C;
+
+//console
+console.log(Add);
+
+//variable declearation
+var A=2;
+var B=2;
+var C=2;
+var div = A/C;
+
+//console
+console.log(div);
+
+//variable declearation
+var A=2;
+var B=1;
+var C=1;
+var subt = A+B+C;
+
+//console
+console.log(subt);
+//var declear
+var b= 6;
+b++;
+console.log(b)
+// colour
